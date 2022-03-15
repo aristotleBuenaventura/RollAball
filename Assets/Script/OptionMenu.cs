1191fcbd79a2e14a208e608959026e149560de4e
Lei <aaristotlee@gmail.com>
2022-03-15 14:37:46 +0800

    All files in RollABall

---
+++ Assets/Script/OptionMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour
+{
+    Resolution[] resolutions;
+    public Dropdown ResolutionDropdown;
+
+
+    void Start()
+    {
+
+        resolutions = Screen.resolutions;
+
+        ResolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(Option);
+
+
+        }
+
+        ResolutionDropdown.AddOptions(options);
+
+    }
+}
